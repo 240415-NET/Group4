5849be00c73cf62d4350e3315477f6374d9966d2
CoreyKnox815 <CoreyKnox815@gmail.com>
2024-06-05 12:37:39 -0400

    added DataContext

--- ActivityTracker/Program.cs
+++ ActivityTracker/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ActivityTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddScoped<IUserService, UserService>(); // This adds our UserService, that our UserController then asks for
+//builder.Services.AddScoped<IUserStorageEFRepo, UserStorageEFRepo>();// This adds our UserStorageEFRepo (data-access layer), that our UserService asks for. 
+
+
+string connectionString = File.ReadAllText(@"../../ConnectionString.txt");
+
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
