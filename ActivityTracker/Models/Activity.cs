5a22bfd109a8fa21f08d878cf76b6e176f50f3b1
CoreyKnox815 <CoreyKnox815@gmail.com>
2024-06-05 12:44:26 -0400

    added stuff to models for ef core mysteries

--- ActivityTracker/Models/Activity.cs
+++ ActivityTracker/Models/Activity.cs
@@ -1,11 +1,15 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace ActivityTracker.Models;
 public class Activity
 {
+    [Key]
     public Guid activityID {get; set;}
     public string activity_Description {get;set;}
     public string nameOfPerson {get;set;}
     public DateOnly Date_OfActivity {get;set;}
     public TimeOnly Time_OfActivity {get;set;}
+    public User user {get; set;} = new();
 
 }
--- ActivityTracker/Models/User.cs
+++ ActivityTracker/Models/User.cs
@@ -1,12 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 
 
 namespace ActivityTracker.Models;
 public class User
 {
+    [Key]
     public Guid userId {get; set;}
     public string userName {get; set;}
     public string userEmail {get; set;}
     public string user_FirstName {get; set;}
     public string user_LastName {get; set;}
+
+    public List<Activity> activityList {get; set;} = new();
  
 }
